83c225b305b2ef4b6143b8b0732e68eb8a7ad615
Anitha <anithaa5299@gmail.com>
2026-04-24 14:24:59 -0400

    added env changes

--- Auth-service/app1/database.py
+++ Auth-service/app1/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@my_db1:5432/authdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
